1a882e94c6e36f9a92684874775643094d887923
Olti Asllanaj <oltias1987@gmail.com>
2020-04-25 19:22:53 -0400

    Updates

---
+++ precache-manifest.be6a1a0e911786a4534b47cadbd27394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54772263cade2859b916e46947dc3aa8",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "309579b1a99b2ebd9e3a",
+    "url": "/RoboFriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "67a038f51c2218c5da5f",
+    "url": "/RoboFriends/static/css/main.fd5ed547.chunk.css"
+  },
+  {
+    "revision": "309579b1a99b2ebd9e3a",
+    "url": "/RoboFriends/static/js/2.b85c6ecd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.b85c6ecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a038f51c2218c5da5f",
+    "url": "/RoboFriends/static/js/main.8f1109b0.chunk.js"
+  },
+  {
+    "revision": "79e0ff11f9ee99630970",
+    "url": "/RoboFriends/static/js/runtime-main.6127e1b7.js"
+  }
+]);
